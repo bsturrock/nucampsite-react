3dce5cd3f57d7bcc56d716f4831140bd704e855d
Brandon Sturrock <brandon.sturrock@outlook.com>
2023-11-18 12:43:37 -0500

    header + footer

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,22 @@
+import {CAMPSITES} from '../../app/shared/CAMPSITES'
+import {Col, Row} from 'reactstrap'
+import CampsiteCard from './CampsiteCard'
+
+const CampsitesList = () => {
+
+    const renderedCampsites = CAMPSITES.map((campsite) => {
+        return (
+            <Col md='5' className='m-4 campsites-col' key={campsite.id}>
+                <CampsiteCard campsite={campsite}/>
+            </Col>
+        )
+    })
+
+    return (
+        <Row className='campsites-row'>
+            {renderedCampsites}
+        </Row>
+    )
+}
+
+export default CampsitesList
