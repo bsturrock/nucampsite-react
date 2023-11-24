20754871624bc204d484e041cda847967914ea30
Brandon Sturrock <brandon.sturrock@dynatrace.com>
2023-11-24 17:04:27 -0500

    campsitesSlice

--- src/features/campsites/CampsitesList.jsx
+++ src/features/campsites/CampsitesList.jsx
@@ -3,7 +3,8 @@ import {Col, Row} from 'reactstrap'
 import CampsiteCard from './CampsiteCard'
 
 const CampsitesList = () => {
-
+    
+    const campsites = selectAllCampsites();
     const renderedCampsites = CAMPSITES.map((campsite) => {
         return (
             <Col md='5' className='m-4 campsites-col' key={campsite.id}>
