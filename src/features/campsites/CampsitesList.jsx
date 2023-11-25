c304806fdd1f62d4872e313aa42456d5ca6b2c5b
Brandon Sturrock <brandon.sturrock@dynatrace.com>
2023-11-25 09:16:11 -0500

    selectCampsitebyId

--- src/features/campsites/CampsitesList.jsx
+++ src/features/campsites/CampsitesList.jsx
@@ -2,12 +2,12 @@ import {Col, Row} from 'reactstrap'
 import CampsiteCard from './CampsiteCard'
 import { selectAllCampsites } from './camspitesSlice';
 
-const CampsitesList = () => {
+const CampsitesList = ({setCampsiteId}) => {
 
     const campsites = selectAllCampsites();
     const renderedCampsites = campsites.map((campsite) => {
         return (
-            <Col md='5' className='m-4 campsites-col' key={campsite.id}>
+            <Col md='5' className='m-4 campsites-col' key={campsite.id} onClick={() => setCampsiteId(campsite.id)}>
                 <CampsiteCard campsite={campsite}/>
             </Col>
         )
