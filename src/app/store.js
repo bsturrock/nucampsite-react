e2144fcf4cb8800e6184114803520b0c594c4d8f
Brandon Sturrock <brandon.sturrock@outlook.com>
2023-12-09 11:20:12 -0500

    middleware

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer
-    }
+    },
+    middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat([logger])
 });
